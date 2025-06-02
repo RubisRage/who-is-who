5b10ac0ca6ededb2c14e035eb31631c27ca2f541
rubisrage <rubensanlor@gmail.com>
2025-06-02 02:31:21 +0100

    Finalmente Maud

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,104 @@
-use askama::Template;
+#![allow(unused_braces)]
+
 use axum::{
-    response::{Html, IntoResponse},
+    response::Html,
     routing::{get, post},
+    Form,
 };
+use maud::html;
+use maud::Markup;
+use serde::Deserialize;
 
-#[derive(Template)]
-#[template(path = "index.html")]
-struct Index {
-    tareas: Vec<Tarea>,
-}
-
-#[derive(Template)]
-#[template(path = "index.html", block = "tarea_frag")]
-struct TareaFrag {
-    tareas: Vec<Tarea>,
-}
-
+#[derive(Debug)]
 struct Tarea {
+    id: &'static str,
+    description: String,
+}
+
+fn render_page(tareas: &[Tarea]) -> Markup {
+    html! {
+        html lang="en" {
+            head {
+                meta charset="UTF-8";
+                meta name="viewport" content="width=device-width, initial-scale=1.0";
+                script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous" {}
+                title { "Who is who" }
+            }
+            body {
+                h1 { "Ejemplo de uso" }
+                main {
+                    section {
+                        h3 { "Lista de tareas" }
+                        ul id="lista" {
+                            @for tarea in tareas {
+                                (render_tarea(tarea))
+                            }
+                        }
+                    }
+                    form {
+                        h2 { "Crear una tarea" }
+                        div {
+                            label for="description" { "Descripción:" }
+                            input #"description" name="description" type="text";
+                        }
+                        button type="submit" hx-post="/nueva" hx-target="#lista" hx-swap="beforeend" {
+                            "Añadir"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn render_tarea(tarea: &Tarea) -> Markup {
+    html! {
+        li id={ (tarea.id) } {
+            p { (tarea.description) }
+        }
+    }
+}
+
+#[axum::debug_handler]
+async fn index() -> Html<String> {
+    let tareas = vec![
+        Tarea {
+            id: "1",
+            description: "Comprar leche".to_string(),
+        },
+        Tarea {
+            id: "2",
+            description: "Wazaaa".to_string(),
+        },
+        Tarea {
+            id: "3",
+            description: "Terminar de comer".to_string(),
+        },
+    ];
+
+    Html(render_page(&tareas).into_string())
+}
+
+#[derive(Deserialize)]
+struct NuevaReq {
     description: String,
 }
 
 #[axum::debug_handler]
-async fn index() -> impl IntoResponse {
-    let template = Index {
-        tareas: vec![
-            Tarea {
-                description: "Comprar cosas de limpieza".to_string(),
-            },
-            Tarea {
-                description: "Otra tarea artificial".to_string(),
-            },
-        ],
+async fn nueva(Form(req): Form<NuevaReq>) -> Html<String> {
+    let tarea = Tarea {
+        id: "estoy habría que generarlo y eso",
+        description: req.description,
     };
 
-    Html(template.render().unwrap().into_response())
-}
-
-#[axum::debug_handler]
-async fn nueva() -> impl IntoResponse {
-    let tareas = vec![
-        Tarea {
-            description: "Comprar cosas de limpieza".to_string(),
-        },
-        Tarea {
-            description: "Otra tarea artificial".to_string(),
-        },
-        Tarea {
-            description: "Tarea de prueba".to_string(),
-        },
-    ];
-
-    Html(TareaFrag { tareas }.render().unwrap())
+    Html(render_tarea(&tarea).into_string())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = axum::Router::new()
         .route("/", get(index))
-        .route("/nueva", post(nueva))
-    ;
+        .route("/nueva", post(nueva));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
