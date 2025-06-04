281f8d2e1d52d6846c68f524c2c38f1191c41764
rubisrage <rubensanlor@gmail.com>
2025-06-04 02:32:01 +0100

    Improve scrapper ergonomics

--- src/scrapper/main.rs
+++ src/scrapper/main.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{fmt::Display, time::Duration};
 
 use reqwest::blocking::Client;
 use scraper::{Html, Selector};
@@ -6,13 +6,25 @@ use scraper::{Html, Selector};
 enum ScrapError {
     Request(reqwest::Error),
     Retry,
-    NotPresent(Kind),
+    NameNotPresent,
     ParseName(String),
 }
 
-enum Kind {
-    Name,
-    Photo,
+impl Display for ScrapError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ScrapError::Retry => write!(f, "Retried too many times!"),
+            ScrapError::Request(e) => {
+                write!(f, "{e}")
+            }
+            ScrapError::NameNotPresent => {
+                write!(f, "Tag <span class=\"texto\"> containing professors name was not present!")
+            }
+            ScrapError::ParseName(text) => {
+                write!(f, "Failed to parse '{text}', name did not contain ','!")
+            }
+        }
+    }
 }
 
 impl From<reqwest::Error> for ScrapError {
@@ -21,54 +33,64 @@ impl From<reqwest::Error> for ScrapError {
     }
 }
 
-fn obtain_professor(
-    url: &str,
-    max_retries: u32,
-    timeout: Duration,
-) -> Result<(String, String), ScrapError> {
-    let mut retries = 0;
-    let res = loop {
-        match Client::new().get(url).timeout(timeout).send() {
-            Ok(res) => break Ok(res),
-            Err(e) => {
-                if e.is_timeout() && retries >= max_retries {
-                    break Err(ScrapError::Retry);
-                } else if !e.is_timeout() {
-                    break Err(e.into());
+struct ScrappedProfessor {
+    name: String,
+    picture: Option<String>,
+}
+
+impl ScrappedProfessor {
+    fn scrap(
+        url: &str,
+        max_retries: u32,
+        timeout: Duration,
+    ) -> Result<Self, ScrapError> {
+        let mut retries = 0;
+        let res = loop {
+            match Client::new().get(url).timeout(timeout).send() {
+                Ok(res) => break Ok(res),
+                Err(e) => {
+                    if e.is_timeout() && retries >= max_retries {
+                        break Err(ScrapError::Retry);
+                    } else if !e.is_timeout() {
+                        break Err(e.into());
+                    }
                 }
-            }
-        };
+            };
 
-        retries += 1;
-    }?;
+            retries += 1;
+        }?;
 
-    let document = Html::parse_document(&res.error_for_status()?.text()?);
+        let document = Html::parse_document(&res.error_for_status()?.text()?);
 
-    let name_selector = Selector::parse("span.texto")
-        .expect("span.texto should be a valid CSS selector");
-    let img_selector =
-        Selector::parse("img").expect("img should be a valid CSS selector");
+        let name_selector = Selector::parse("span.texto")
+            .expect("span.texto should be a valid CSS selector");
+        let img_selector =
+            Selector::parse("img").expect("img should be a valid CSS selector");
 
-    let name_text = document
-        .select(&name_selector)
-        .next()
-        .ok_or(ScrapError::NotPresent(Kind::Name))?
-        .inner_html();
+        let name_text = document
+            .select(&name_selector)
+            .next()
+            .ok_or(ScrapError::NameNotPresent)?
+            .inner_html();
 
-    let (surname, firstname) = name_text
-        .split_once(",")
-        .map(|(surname, firstname)| (surname.trim(), firstname.trim()))
-        .ok_or_else(|| ScrapError::ParseName(name_text.clone()))?;
+        let (surname, firstname) = name_text
+            .split_once(",")
+            .ok_or_else(|| ScrapError::ParseName(name_text.clone()))?;
 
-    let name = format!("{firstname} {surname}");
+        let name = format!("{} {}", firstname.trim(), surname.trim());
 
-    let photo = document
-        .select(&img_selector)
-        .filter_map(|img| img.attr("src").filter(|src| src.starts_with("foto")))
-        .next()
-        .ok_or(ScrapError::NotPresent(Kind::Photo))?;
+        let picture = document
+            .select(&img_selector)
+            .filter_map(|img| {
+                img.attr("src")
+                    .filter(|src| src.starts_with("fotos"))
+                    .filter(|src| *src != "fotos/logo.gif")
+                    .map(From::from)
+            })
+            .next();
 
-    Ok((name, photo.into()))
+        Ok(ScrappedProfessor { name, picture })
+    }
 }
 
 fn main() {
@@ -80,23 +102,20 @@ fn main() {
             format!("https://www.dis.ulpgc.es/profesorado/ficha.asp?id={i}");
 
         print!("Page({i}): ");
-        match obtain_professor(&url, max_retries, timeout) {
-            Ok((name, photo)) => {
-                println!("Professor has name: '{name}' and photo: '{photo}'")
+
+        match ScrappedProfessor::scrap(&url, max_retries, timeout) {
+            Ok(ScrappedProfessor {
+                name,
+                picture: Some(picture),
+            }) => {
+                println!("Professor has name: '{name}' and photo: '{picture}'")
             }
-            Err(ScrapError::Request(e)) => {
-                println!("{e}");
+
+            Ok(ScrappedProfessor { name, picture: _ }) => {
+                println!("Professor has name: '{name}' and no photo")
             }
-            Err(ScrapError::Retry) => println!("Retried too many times"),
-            Err(ScrapError::NotPresent(Kind::Name)) => {
-                println!("Name was not present")
-            }
-            Err(ScrapError::NotPresent(Kind::Photo)) => {
-                println!("Photo was not present")
-            }
-            Err(ScrapError::ParseName(text)) => {
-                println!("Failed to parse {text}")
-            }
+
+            Err(e) => println!("{e}")
         }
     }
 }
